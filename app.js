21f6064714158984d651f106a1cfd5caa2dfe421
Dhananjay Sathe <dhananjaysathe@gmail.com>
2014-03-24 20:53:13 +0530

    cleanup old kibana code

--- app.js
+++ app.js
@@ -48,33 +48,3 @@ function run() {
   console.log('Server listening on ' + config.listen_port);
 }
 
-function kibana3configjs(req, res) {
- 
-
-  function getKibanaIndex() {
-    var raw_index = config.kibana_es_index;
-    var user_type = config.which_auth_type_for_kibana_index;
-    var user;
-    if (raw_index.indexOf('%user%') > -1) {
-      if (user_type === 'google') {
-        user = req.googleOauth.id;
-      } else if (user_type === 'basic') {
-        user = req.user;
-      } else if (user_type === 'cas') {
-        user = req.session.cas_user_name;
-      } else {
-        user = 'unknown';
-      }
-      return raw_index.replace(/%user%/gi, user);
-    } else {
-      return raw_index;
-    }
-  }
-
-  res.setHeader('Content-Type', 'application/javascript');
-  res.end("define(['settings'], " +
-    "function (Settings) {'use strict'; return new Settings({elasticsearch: '/__es', default_route     : '/dashboard/file/default.json'," +
-      "kibana_index: '" +
-      getKibanaIndex() +
-      "', panel_names: ['histogram', 'map', 'pie', 'table', 'filtering', 'timepicker', 'text', 'hits', 'column', 'trends', 'bettermap', 'query', 'terms', 'sparklines'] }); });");
-}
